7f85b2625a35839fbe2356b3db837c77097aca32
EmersonSouza1 <56404529+EmersonSouza1@users.noreply.github.com>
2026-02-14 16:42:17 -0300

    fix: replace binary favicons with svg assets for PR compatibility

---
+++ libs/shared/contracts/src/lib/contracts/contracts.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-contracts',
+  imports: [],
+  templateUrl: './contracts.html',
+  styleUrl: './contracts.scss',
+})
+export class Contracts {}
